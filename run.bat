0638c0e7d6fe1417339621cdcbbe81660747e29b
PiyushDatta <piyushdattaca@gmail.com>
2024-02-21 18:55:51 -0500

    Fixed the run commands for windows. Also added a template main file so we can continuously just keep copying the file to main.cc when we write code in main.cc

--- run.bat
+++ run.bat
@@ -1,5 +1,8 @@
 @echo off
 
 REM run command
-g++ main.cc -o "out/main.out" && "out/main.out"
-g++ check.cc -o "out/check.out" && "out/check.out"
+if not exist "out" ( 
+    mkdir "out"
+)
+g++ main.cc -o "out/main.out" & "out/main.out"
+g++ check.cc -o "out/check.out" & "out/check.out"
---
+++ template_main.cc
@@ -0,0 +1,48 @@
+// cpp template test file
+#include "bits/stdc++.h"
+using namespace std;
+
+// DEFINE
+#define _MY_TEST_FILES true
+
+// MACROS
+#define ll long long
+#define _debug(x) cout << x << endl
+
+void solve() {
+  // int n = 0;
+  // cin >> n;
+  // int k = 0;
+  // cin >> k;
+
+  // vector<int> a(n, 0);
+  // for (int &it : a) {
+  //   cin >> it;
+  // }
+}
+
+// Driver program to test above function
+int main() {
+  // optional performance optimizations
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+#ifdef _MY_TEST_FILES
+  // for getting input from input.txt file
+  freopen("input.txt", "r", stdin);
+  // printing the output to output.txt file
+  freopen("my_ans.txt", "w", stdout);
+#endif
+
+  // keep this, testcases
+  int t;
+  cin >> t;
+
+  // now we can run our algo
+  for (int i = 1; i <= t; ++i) {
+    // cout << "Case #" << i << ": ";
+    solve();
+  }
+
+  return 0;
+}
